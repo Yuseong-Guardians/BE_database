f65c12dd39f3d3fc7d92b7ea47fa6845f786c979
Yu-Jaeyoung <jackyu0721@gmail.com>
2024-11-07 12:28:56 +0900

    config file modify

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'yuseong-app', // 애플리케이션 이름
-      script: 'pnpm', // pnpm을 사용하여 실행
-      args: 'start', // nestjs start 명령어 실행
-      cwd: './', // 프로젝트 루트 디렉토리
-      interpreter: 'node', // node.js를 interpreter로 사용
-      env: {
-        NODE_ENV: 'production',
-      },
+      name: 'yuseong-app', 
+      script: 'pnpm', 
+      args: 'start',
+      cwd: './',
+      interpreter: 'node',
+      watch: true,
     },
   ],
 };
